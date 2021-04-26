d0760cbaa9999cdd022d1fd121bd0db21ca9931a
Alvin Muniz <muniz.alvin@gmail.com>
2021-04-26 10:08:11 -0500

    hello service updated with base url

--- src/app/hello-world.service.ts
+++ src/app/hello-world.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Message } from './model/message';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelloWorldService {
 
-  constructor() { }
+  BASE_URL="http://localhost:9092/auth/users/register";
+
+  constructor(private http: HttpClient) {
+  }
+
+    executeHelloWorldService() {
+      return this.http.get<Message>(this.BASE_URL);
+    }
 }
