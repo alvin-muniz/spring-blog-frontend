e5adc6ea40396d30ee0ab7e8e88e83c55ab84d77
Alvin Muniz <muniz.alvin@gmail.com>
2021-04-28 09:53:07 -0500

    added user form routing and rendered on front end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HelloWorldComponent} from './hello-world/hello-world.component';
-
+import {UserFormComponent} from './user-form/user-form.component';
 
 
 const routes: Routes = [
   // {path: '', component: HelloWorldComponent},
   {path: 'hello-world', component: HelloWorldComponent},
+  {path: 'user-form', component: UserFormComponent}
 ];
 
 @NgModule({
